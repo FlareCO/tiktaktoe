1ffacde3200c2bd5da87322630b46b85911f66b9
Marlon <31259698+a100w@users.noreply.github.com>
2020-03-03 22:53:51 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * TicTacToe Configuration
+ * Last update: 03.03.2020
+ */
+
+// MySQL
+$conf_mysql_addr    =   'basten-it.eu'; // MySQL-Server | IP-Adresse
+$conf_mysql_user    =   'tictactoe';    // MySQL-Server | Benutzer
+$conf_mysql_pass    =   'rV!9f44w'; // MySQL-Server | Benutzer Passwort
+$conf_mysql_data    =   'tictactoe';    // MySQL-Server | Datenbank
